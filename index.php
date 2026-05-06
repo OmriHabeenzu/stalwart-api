ac4f62a8bdf851fbf4c68275eabf2a62826bbca3
Omri Habeenzu <omri@stalwartzm.com>
2026-05-06 12:47:33 +0200

    Remove emergency comment from index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// STALWART API - EMERGENCY MINIMAL VERSION
+// STALWART API
 require_once __DIR__ . '/vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception as PHPMailerException;
